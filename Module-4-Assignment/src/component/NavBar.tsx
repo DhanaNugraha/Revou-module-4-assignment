2aa8cf270d1c601a98f33577561397e5f6198a9d
DhanaNugraha <dhananugraha1511@gmail.com>
2025-01-16 20:43:32 +0700

    feat: changing navbar from tailwind to css. adjusting the css for the navbar.

--- Module-4-Assignment/src/component/NavBar.tsx
+++ Module-4-Assignment/src/component/NavBar.tsx
@@ -6,14 +6,10 @@ const NavBar = () => {
   const isLoggedIn = Boolean(localStorage.getItem("access_token"));
   const location = useLocation()
 
-  const buttonStyling = "border text-[1em] font-medium bg-[#1a1a1a] cursor-pointer transition-[border-color] duration-[0.25s] px-[1em] py-[0.8em] rounded-lg border-solid border-transparent hover:border-[#646cff]"
-
-  const currentPageButtonStyling = "border text-[1em] font-medium bg-blue-400 cursor-pointer transition-[border-color] duration-[0.25s] px-[1em] py-[0.8em] rounded-lg border-solid border-transparent hover:border-[#646cff]"
-
   const checkLocation = (path:any) => {
     return location.pathname === path
-    ? currentPageButtonStyling
-    : buttonStyling
+    ? "navbarCurrentPageContentStyling"
+    : "navbarContentStyling"
   }
 
   const handleLogout = () => {
@@ -22,20 +18,19 @@ const NavBar = () => {
     alert("Successfully logged out")
   }
 
-  
-  
-
   return (
-    <div className="flex justify-around pt-[2%] pb-[2%] border-b border-solid">
+    <div className="navbarContainer">
       <h1>Shop Free</h1>
       <Link className={checkLocation("/")} to="/">Home</Link>
       <Link className={checkLocation("/ShoppingCart")} to="/ShoppingCart">Cart</Link>
       {isLoggedIn ? (
-        <button onClick={handleLogout} className={buttonStyling}>Logout</button>
+        <button onClick={handleLogout} className={checkLocation("/Login")}>Logout</button>
       ) : (
-        <Link className={checkLocation("/Login")} to="/Login">Login</Link>
+        <div className="navBarLoginButtons">
+          <Link className={checkLocation("/Login")} to="/Login">Login</Link>
+          <Link className={checkLocation("/Register")} to="/Register">Register</Link>
+        </div>
       )}
-      <Link className={checkLocation("/Register")} to="/Register">Register</Link>
     </div>
   );
 };
